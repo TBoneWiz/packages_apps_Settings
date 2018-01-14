6eb4f508e77ae81ee95b70f2e8473186bee18f9c
Michael W <baddaemon87@gmail.com>
2018-01-14 19:13:11 +0100

    DevelopmentSettings: Fix possible NPE
    * getSystemService() can return null if the service is not
      available
    * Check this before accessing a method
    * Edge case: This code is usually not called on every device
    
    Change-Id: I722638f2647dde7fb7d6a9a29c4a0e82314231c7

--- src/com/android/settings/Utils.java
+++ src/com/android/settings/Utils.java
@@ -872,7 +872,7 @@ public final class Utils {
     static boolean isOemUnlockEnabled(Context context) {
         PersistentDataBlockManager manager =(PersistentDataBlockManager)
                 context.getSystemService(Context.PERSISTENT_DATA_BLOCK_SERVICE);
-        return manager.getOemUnlockEnabled();
+        return manager != null && manager.getOemUnlockEnabled();
     }
 
     /**
